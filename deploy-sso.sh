c41614a0534b60e3b5c55eac4ac839ae9df361c3
Hugo Pfeffer <hugo.pguima@gmail.com>
2021-11-22 18:46:26 -0300

    add: script for deploying the need resources

--- deploy-sso.sh
+++ deploy-sso.sh
@@ -1,4 +1,5 @@
-userNumber=2
+userNumber=1
+oc new-project sso${userNumber}
 
 oc new-app \
     --template=sso73-ocp4-x509-https \
@@ -15,16 +16,17 @@ oc new-app \
     --name=js-console \
     -n sso${userNumber}
 
-  # oc new-app \
-  #   mailhog/mailhog \
-  #   --name=mailhog \
-  #   -n sso${userNumber}
+  oc new-app \
+    mailhog/mailhog \
+    --name=mailhog \
+    -n sso${userNumber}
 
-  # oc expose svc js-console -n sso${userNumber}
+  oc expose svc js-console -n sso${userNumber}
   # oc expose svc mailhog --target-port=8025-tcp -port 8025 -n sso${userNumber}
+  oc expose svc mailhog --target-port=8025-tcp -n sso${userNumber}
 
-  # oc label deploy js-console app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
-  # oc label dc sso app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
-  # oc label deploy mailhog app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
-  # oc annotate deploy js-console app.openshift.io/connects-to=sso73-ocp4-x509-https -n sso${userNumber}
-  # oc annotate dc sso app.openshift.io/connects-to=mailhog -n sso${userNumber}
+  oc label deploy js-console app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
+  oc label dc sso app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
+  oc label deploy mailhog app.kubernetes.io/part-of="redhat-sso" -n sso${userNumber}
+  oc annotate deploy js-console app.openshift.io/connects-to=sso73-ocp4-x509-https -n sso${userNumber}
+  oc annotate dc sso app.openshift.io/connects-to=mailhog -n sso${userNumber}
